5059f19f93a400691ca4dcd9371ecf90709e5761
nohsoobin <nohsoobin18@naver.com>
2022-12-11 02:00:55 +0900

    asad
    Signed-off-by: nohsoobin <nohsoobin18@naver.com>

--- pages/myapp.js
+++ pages/myapp.js
@@ -8,28 +8,15 @@ export default function Home() {
       <div className="py-2">
         <Link href="/">bookweb home</Link>
       </div>
-
-      <div className={styles.text}>
-        <p>
-          <p>
-            <a href="https://huhuhu-phi.vercel.app/">노수빈 개인 포트폴리오</a>
-          </p>
-          <p>
-            <a href="https://mymy-hanyooja.vercel.app/">
-              한유정 개인 포트폴리오
-            </a>
-          </p>
-          <p>
-            <a href="https://mymy-beige.vercel.app/">임혜준 개인 포트폴리오</a>
-          </p>
-          <p>
-            <a href="https://myblog-opal.vercel.app/">양유나 개인 포트폴리오</a>
-          </p>
-
-          <div className={styles.img}>
-            <img src="/images/123123.jpg" />
-          </div>
-        </p>
+      <div>
+        <h2>노수빈 개인 포트폴리오</h2>
+        <a href="https://huhuhu-phi.vercel.app">NOH</a>
+        <h2>한유정 개인 포트폴리오</h2>
+        <a href="https://mymy-hanyooja.vercel.app">HAN</a>
+        <h2>임혜준 개인 포트폴리오</h2>
+        <a href="https://mymy-beige.vercel.app">LIM</a>
+        <h2>양유나 개인 포트폴리오</h2>
+        <a href="https://myblog-opal.vercel.app">YANG</a>
       </div>
     </Layout>
   )
